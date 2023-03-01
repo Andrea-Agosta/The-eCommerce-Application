cfa61bdf975832e5452f40ec7c225c974bb9aba9
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-03-01 12:25:36 +0100

    update delete item from store if you are admin

--- client/src/components/Modal/ButtonGroup.tsx
+++ client/src/components/Modal/ButtonGroup.tsx
@@ -5,11 +5,14 @@ interface IButtonGroup {
 }
 
 export const ButtonGroup = ({ handleClose, submitData, name }: IButtonGroup) => {
+  const deleteButtonStyle: string = "inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm";
+  const submitButtonStyle: string = "inline-flex w-full justify-center rounded-md border border-transparent bg-orange-400 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+
   return (
     <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
       <button
         type="button"
-        className="inline-flex w-full justify-center rounded-md border border-transparent bg-orange-400 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+        className={name === 'Delete Item' ? deleteButtonStyle : submitButtonStyle}
         onClick={() => submitData()}
       > {name} </button>
       <button
